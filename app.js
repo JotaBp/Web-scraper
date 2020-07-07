47f3d48998114f83616cf3530387bb33a8bb20ad
Julian Bajo <jbajopinedo@gmail.com>
2020-07-07 18:01:06 +0200

    ready

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+// Initialize Express
+const app = express();
+
+// Configure middleware
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Make public a static folder
+app.use(express.static("public"));
+
+// Simple index route
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "./public/index.html"));
+  });
+  
+// Start the server
+app.listen(PORT, function() {
+  console.log("App listening on port " + PORT);
+});
